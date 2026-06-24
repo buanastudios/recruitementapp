b6b0e11e1e7a1eee7bfb1e476e5ddaabaabe4a0d
Hikmatullah Sakti <hikmatullahsakti@gmail.com>
2026-06-24 11:13:29 +0700

    change from opensheet to googleapps

--- config.js
+++ config.js
@@ -2,3 +2,6 @@ const SHEET_ID = "1bluduila5k0tTi8YGgjpEkp_9O6Kwewv8ag7xlpwF94";
 
 const API_URL =
 `https://opensheet.elk.sh/${SHEET_ID}/Candidate`;
+
+
+API_URL = "https://script.google.com/macros/s/AKfycbyPjD9oxPcLZlyjin9fNEbIu6BwVdpYiuUlLXzMQUwbfvjRlPw3ucjheKKJBIAQBSvVWQ/exec"
